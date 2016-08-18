5742e3a978dd81592bb9522191ba87b8e176927a
squall2229 <squall2229@gmail.com>
2016-08-18 22:21:02 +0300

    Сделал jsonp и условия проверки на функцию и строку

--- src/pictures.js
+++ src/pictures.js
@@ -1,14 +1,20 @@
 'use strict';
-var pictures = [];
-var scripUrl = 'http://localhost:1506/api/pictures?callback=jsonpCallback';
-var scriptEl = document.createElement('script');
-var jsonpCallback = function(data) {
-  pictures = data;
-};
-
-var picturesLoad = function(url, callbackData) {
-  scriptEl.src = url;
-  document.body.appendChild(scriptEl);
-  callbackData;
-};
-picturesLoad(scripUrl, jsonpCallback(pictures));
+(function() {
+  window.CallbackRegistry = {};
+  var picturesUrl = '/api/pictures';
+  var pictures = null;
+  var picturesCallback = function(data) {
+    pictures = data;
+  };
+  var loadJsonp = function(url, callback) {
+    if (typeof (callback) === 'function' && typeof (url) === 'string') {
+      var callbackName = 'cb' + String(Math.random()).slice(-6);
+      window.CallbackRegistry[callbackName] = callback;
+      url += '?callback=CallbackRegistry.' + callbackName;
+      var picturesScript = document.createElement('script');
+      picturesScript.src = url;
+      document.body.appendChild(picturesScript);
+    }
+  };
+  loadJsonp(picturesUrl, picturesCallback);
+}());
